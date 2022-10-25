8587a16265907a565cd1c9d263a00c07b528c38b
Priscila Franco <francopri@hotmail.com>
2022-10-25 18:37:10 -0300

    lab resolvido

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    //printtimecallback é uma função que vem do index.js.  Precisamos checar se essa função realmente foi passada.  Caso a função tenha sido passada ->>> vamos invocar a função
+
+    this.intervalId = setInterval(() => {
+
+      //should increment by 1 the currentTime property every 1 second
+      this.currentTime++;
+
+      //checando se printimecallback existe
+      if (printTimeCallback)
+
+        //sim ela existe
+        printTimeCallback();
+
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime) / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // return value > 9 ? '' + value : '0' + value;
+    // return (value + '').padStart(2,'0');
+    //return String(value).padStart(2,'0');
+
+    return value.toString().padStart(2, '0');
+
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+
   }
 }
